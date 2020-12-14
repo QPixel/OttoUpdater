cd8d0dfa4871b3bbfe92e7f9ff6a554405ac6b3e
Riley Smith <riley@rileysmith.me>
2020-12-14 12:47:33 -0800

    Seperated UI and Downloader

---
+++ auth.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Riley Smith
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package main
+
+func authWithMojang() {
+
+}
--- launcher.go
+++ launcher.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"flag"
 	"net/http"	
 	"time"
 	"math/rand"
-	"github.com/Equanox/gotron"
 )
 
 var httpClient = &http.Client{}
-
-var (
-	manifestID 	string
-	installPath 	string
-	gamePath 	string
-	workerCount	 int
-)
 
 const defaultDownloadURL = "https://api.qpixel.me/api/v1/ottomods?version=latest"
 
@@ -24,7 +15,7 @@ func init() {
 	rand.Seed(time.Now().Unix())
 
 	// Parse Flags
-	flag.StringVar(&manifestID, "manifest", "", "download a specific manifest")
+	// flag.StringVar(&manifestID, "manifest", "", "download a specific manifest")
 	// flag.StringVar(&)
 	// Set http timeout
 	httpClient.Timeout = 30 * time.Second
@@ -32,27 +23,4 @@ func init() {
 
 
 func main() {
-	// Make working directories
-	// if gamePath != "" {
-	// 	os.MkdirAll(gamePath, os.ModePerm)
-	// }
-	window, err := gotron.New()
-	if err != nil {
-		panic(err)
-	}
-	// set default window size
-	window.WindowOptions.Width = 1200
-	window.WindowOptions.Height = 980
-	window.WindowOptions.Title = "OttoUpdater"
-
-	// Start the browser window
-	done, err := window.Start()
-	if err != nil {
-		panic(err)
-	}
-
-	//Open dev tools after window .start
-	window.OpenDevTools()
-
-	<-done
 }
